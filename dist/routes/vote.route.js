248934cb24dcbedf54361ec809db68f2e997ddde
rdndany <iliedanielmarian@gmail.com>
2025-03-28 10:29:06 +0200

    Stop tracking dist folder

--- dist/routes/vote.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const vote_controller_1 = require("../controllers/vote.controller");
-const voteRoutes = (0, express_1.Router)();
-voteRoutes.get("/:coin_id", vote_controller_1.getByCoinId);
-voteRoutes.post("/:coin_id", vote_controller_1.voteByCoinId);
-voteRoutes.get("/hasVoted/:coin_id", vote_controller_1.hasVotedToday);
-exports.default = voteRoutes;
--- dist/utils/request.utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.extractClientIP = extractClientIP;
-function extractClientIP(req) {
-    var _a;
-    const rawIp = String(req.headers["x-forwarded-for"] || req.socket.remoteAddress);
-    return ((_a = rawIp.split(",").pop()) === null || _a === void 0 ? void 0 : _a.trim()) || "";
-}
